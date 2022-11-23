e21131cd8a5c930ce938e37b7aca007238fa547b
RemigiuszZydek <zyduremik@gmail.com>
2022-11-23 10:37:15 +0100

    deleted /* */

--- src/meme_type.ts
+++ src/meme_type.ts
@@ -1,18 +1,3 @@
-/*export class meme  {
-    url:string|undefined;
-    title:string;
-    likes:string|undefined;
-    dislikes: string|undefined;
-    autor:string;
-
-    constructor(url: string,title: string,likes: string,dislikes: string,autor: string){
-      this.url=url;
-      this.title=title;
-      this.likes=likes;
-      this.dislikes=dislikes;
-      this.autor=autor;
-    }
-  };*/
   export interface meme {
     url:string|undefined;
     title:string;
